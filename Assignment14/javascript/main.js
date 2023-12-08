1187995621b58cd934ae841105ebf19a62f70c9b
Kayla Kallis <143760376+kkallis0711@users.noreply.github.com>
2023-12-08 16:37:38 -0700

    Update main.js

--- Assignment14/javascript/main.js
+++ Assignment14/javascript/main.js
@@ -1,6 +1,12 @@
-var c=document.getElementByld("myCanvas");
-var ctx=c.getContext("2d");
-// first line
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
-ctx.stroke(); 
+var c = document.getElementByld("myCanvas");
+var ctx = c.getContext("2d");
+var cty = c.getContext("2d"); 
+
+//fill text
+ctx.font = '30pt Bodoni':
+ctx.strokeText(Picture This', 100, 600);
+
+//Author
+ctx.font = '20pt Bodoni';
+ctx.fillStyle = 'black';
+ctx.fillText('by Alaina Urquhart', 200, 600);
